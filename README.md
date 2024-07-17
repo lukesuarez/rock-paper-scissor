64b5e5db1dcea4118e0794d162e6bcdd66e38309
lukesuarez <lukesuarez93@gmail.com>
2024-07-16 22:31:51 -0300

    Adding functions to play the game on console and updated README file

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# rock-paper-scissor
-rock paper scissor game using javascript
+# Rock, paper and Scissor game
+This project consists of rock paper scissor game using javascript!
+
+Rules:
+
+- Rock beats Paper
+- Paper beats Rock
+- Scissor beats Paper
+- Rock beats Scissor
+
+How to Play:
+
+- Open your Developer tools in a web browser with CTRL + SHIT + C and select "console" .
+- A prompt will appear, then select the following option: Rock, Paper or Scissors.
+- The winner of the round will be show in console log.
+- If you win more rounds than the computer, you will win the round, if you lose or get a draw, you can try again refreshing the page(type "F5")
--- index.js
+++ index.js
@@ -1,1 +1,62 @@
-console.log('Hello World')
+function playGame(){
+
+function getComputerChoice() {
+  let randomChoices = Math.floor(Math.random() * 3)
+  // console.log(randomChoices)
+  if(randomChoices === 0 ) {
+    return 'rock'
+  } else if (randomChoices === 1 ) {
+    return 'paper'
+  } else if (randomChoices === 2 ){
+    return 'scissors'
+  }
+}
+
+function getHumanChoice() {
+  let humanChoice = prompt('Hello! Please enter the following options: Rock or Paper or Scissors')
+  if(humanChoice === 'rock' || 'paper' || 'scissors') {
+    humanChoice = humanChoice.toLowerCase()
+  } 
+  return humanChoice;
+}
+
+    let humanScore = 0
+    let computerScore = 0
+
+function playRound(humanChoice, computerChoice) {
+    
+    if(humanChoice === computerChoice){
+      console.log('DRAW! You selected the same option as the opponent!')
+      humanScore++
+      computerScore++ 
+      console.log('----------------')
+      } else if ((humanChoice === 'rock' && computerChoice === 'scissors') || (humanChoice === 'paper' && computerChoice === 'rock') ||  (humanChoice === 'scissor' && computerChoice === 'paper' )) {
+        console.log(`You WIN! ${humanChoice} beats ${computerChoice}! `)
+        console.log('----------------')
+        humanScore++
+      } else {
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}! `)
+        console.log('----------------')
+        computerScore++
+      }
+    }
+
+
+  for (let i = 1; i <=5 ; i++) {
+    let humanSelection = getHumanChoice()
+    let computerSelection = getComputerChoice();
+    console.log('Player CHOICE: '+ humanSelection)
+    console.log('Computer CHOICE: '+computerSelection)
+    console.log(`Game round: ${i}`)
+    playRound(humanSelection,computerSelection)
+  }
+  if ( humanScore > computerScore) {
+    console.log('You win the game! Congratulations!')
+  } else if ( computerScore > humanScore ) {
+    console.log('You lose the game! try again!')
+  }else if (computerScore === humanScore) {
+    console.log('Draw! Please refresh page(F5) and try again')
+  }
+}
+
+playGame()
